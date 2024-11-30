81372c99d9f68dbdafcf096cc7fd738cf89abec4
Ibrahim77890 <ibrahimqaiser2405@gmail.com>
2024-11-30 15:45:57 +0500

    Initial Commit

---
+++ src/expressivo/Expression.java
@@ -0,0 +1,19 @@
+package expressivo;
+
+public interface Expression {
+
+    public static Expression parse(String input) {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override 
+    public String toString();
+
+    @Override
+    public boolean equals(Object thatObject);
+    
+
+    @Override
+    public int hashCode();
+    
+}
